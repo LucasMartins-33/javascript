0df2d86f0d3b80b42f2911ba7bf05a8f6a82efd7
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2024-07-21 18:36:51 -0300

    exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} KM/H`)
+
+if (vel > 60) { //Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+
+console.log(`Dirijaa sempre com cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,11 @@
+var país = 'França'
+
+console.log(`Vivendo em ${país}`)
+
+if (país == 'Brasil') {
+    console.log('Você é brasileiro')
+}
+
+else {
+    console.log('Você é estrangeiro!')
+}
