7587c23b505244bb47d4bc3dcf928a5b256e6a20
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2024-10-23 14:23:57 -0300

    atividades

---
+++ aula14/estrutura de repetição com variável de controle.js
@@ -0,0 +1,13 @@
+/*
+let contador = 1
+while (contador <= 5) {
+    console.log(contador)
+    contador++
+}
+*/
+
+//--------------------------------------------------
+
+for (let contador = 1; contador <=5; contador++) {
+    console.log(contador)
+}
