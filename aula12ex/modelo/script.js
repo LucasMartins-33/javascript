406379fd9de2cae782a5daaa98f2db036d34d365
Lucas Martins <159872673+LucasMartins-33@users.noreply.github.com>
2024-10-25 09:35:40 -0300

    aulas js

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -3,23 +3,6 @@
 
 
 
-let texto = "Olá, mundo!";
-console.log(texto.length); // Resultado: 11 (conta os caracteres, incluindo espaços e pontuação)
-
-
-
-
-
-let numeros = [1, 2, 3, 4, 5];
-console.log(numeros.length); // Resultado: 5 (o array tem 5 elementos)
-
-
-
-
-
-let input = document.querySelector('input#campo');
-console.log(input.value.length); // Retorna a quantidade de caracteres digitados no campo de input
-
 
 
 
